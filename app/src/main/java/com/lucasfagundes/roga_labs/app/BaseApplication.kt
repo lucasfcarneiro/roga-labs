349657bccd2377b37d24cdd23714132a24f18b16
Lucas <lukasfcarneiro@gmail.com>
2021-07-27 17:26:09 -0300

    Dependencies and plugins updated

---
+++ app/src/main/java/com/lucasfagundes/roga_labs/app/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.lucasfagundes.roga_labs.app
+
+import android.app.Application
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        
+    }
+}
