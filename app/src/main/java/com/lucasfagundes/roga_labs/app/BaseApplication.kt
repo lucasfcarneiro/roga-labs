27189279a5d019f953b0a4fe6e4a875c1a3f97f5
Lucas <lukasfcarneiro@gmail.com>
2021-07-28 16:34:18 -0300

    Koin and Binding setup

--- app/src/main/java/com/lucasfagundes/roga_labs/app/BaseApplication.kt
+++ app/src/main/java/com/lucasfagundes/roga_labs/app/BaseApplication.kt
@@ -1,10 +1,18 @@
 package com.lucasfagundes.roga_labs.app
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        
+        startKoin {
+            modules(
+                listOf(
+
+                )
+            ).androidContext(applicationContext)
+        }
     }
 }
